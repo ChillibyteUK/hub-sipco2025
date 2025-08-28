b23318050aeb1d7ac5ba6e4d6cdb10fe38483f31
ChillibyteUK <dan@chillibyte.co.uk>
2025-08-28 12:36:27 +0100

    show teams in people

--- inc/cb-taxonomies.php
+++ inc/cb-taxonomies.php
@@ -26,7 +26,7 @@ function cb_register_taxes() {
         'show_in_nav_menus'  => true,
         'show_tagcloud'      => false,
         'show_in_quick_edit' => true,
-        'show_admin_column'  => false,
+        'show_admin_column'  => true,
         'show_in_rest'       => true,
         'rewrite'            => false,
     );
